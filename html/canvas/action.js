3aea013a9196e9f8578428c0445126080b41ba1f
qifan_ma <qifan_ma@huace.cn>
2024-01-02 19:55:07 +0800

    feat: canvas添加缩放

--- html/canvas/action.js
+++ html/canvas/action.js
@@ -1,11 +1,20 @@
 var canvasHistory = [];
 
 var brush = {
+  baseOffset: {
+    x: 0,
+    y: 0,
+    scale: 1,
+    origin: {
+      x: 0,
+      y: 0,
+    },
+  },
   x: 0,
   y: 0,
   radius: 0,
   isDrawing: false,
-  drawType: "",
+  drawType: "base",
   color: "rgb(0,0,0)",
   drawLine: function (x, y) {
     ctx.beginPath();
@@ -20,6 +29,17 @@ var brush = {
     ctx.fillStyle = this.color;
     ctx.fill();
   },
+  drawText: function (x, y) {
+    ctx.beginPath();
+    ctx.font = "16px Arial";
+    ctx.fillStyle = this.color;
+    ctx.fillText(brush.drawType, this.x, this.y);
+  },
+  transform: function (e) {
+    var { x, y, scale } = this.baseOffset;
+    canvas.style.transformOrigin = `${brush.baseOffset.origin.x}px ${brush.baseOffset.origin.y}px`;
+    canvas.style.transform = `translate(${x}px, ${y}px) scale(${scale})`;
+  },
 };
 
 /**
@@ -30,40 +50,45 @@ function typeChange(e) {
 }
 
 /**
- * 保存当前画布
- */
-function saveCanvas() {
-  const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  canvasHistory.push(data);
-}
-
-/**
- * 重绘
- */
-function redrawCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  const len = canvasHistory.length;
-  if (len > 0) {
-    ctx.putImageData(canvasHistory[len - 1], 0, 0);
-  }
-}
-
-/**
- * 撤销
- */
-function undoCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  const len = canvasHistory.length;
-  if (len > 0) {
-    ctx.putImageData(canvasHistory.pop(), 0, 0);
-  }
-}
-
-/**
  * actions
  */
-
 const actions = {
+  base: {
+    mousedown(e) {
+      brush.x = e.offsetX;
+      brush.y = e.offsetY;
+      brush.baseOffset.origin.x = 0;
+      brush.baseOffset.origin.y = 0;
+      brush.isDrawing = true;
+    },
+    mouseup(e) {
+      brush.isDrawing = false;
+    },
+    mousemove(e) {
+      if (!brush.isDrawing) {
+        return;
+      }
+      let baseOffset = brush.baseOffset;
+      let w = canvas.width - baseOffset.scale * canvas.width;
+      let h = canvas.height - baseOffset.scale * canvas.height;
+      brush.baseOffset.x += e.offsetX - brush.x;
+      brush.baseOffset.y += e.offsetY - brush.y;
+      brush.baseOffset.x = Math.min(Math.max(brush.baseOffset.x, w), 0);
+      brush.baseOffset.y = Math.min(Math.max(brush.baseOffset.y, h), 0);
+      brush.transform();
+    },
+    mouseleave(e) {
+      brush.isDrawing = false;
+    },
+    wheel(e) {
+      console.log(e);
+      let scale = brush.baseOffset.scale + (e.deltaY > 0 ? 0.1 : -0.1);
+      brush.baseOffset.scale = Math.min(Math.max(scale, 1), 10);
+      brush.baseOffset.origin.x = "center";
+      brush.baseOffset.origin.y = "center";
+      brush.transform();
+    },
+  },
   // 线
   line: {
     mousedown(e) {
@@ -143,4 +168,7 @@ canvas.addEventListener("mouseup", (e) => {
 });
 canvas.addEventListener("mouseleave", function (e) {
   actions[brush.drawType]?.mouseleave(e);
+});
+canvas.addEventListener("wheel", function (e) {
+  actions[brush.drawType]?.wheel(e);
 });
--- html/canvas/canvas.js
+++ html/canvas/canvas.js
@@ -5,5 +5,56 @@ function init() {
   if (canvas.getContext) {
     ctx = canvas.getContext("2d");
     ctx.willReadFrequently = true;
+    loadImage("./imgs/map.jpg");
   }
+}
+
+/**
+ * 保存当前画布
+ */
+function saveCanvas() {
+  const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  canvasHistory.push(data);
+}
+
+/**
+ * 重绘
+ */
+function redrawCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const len = canvasHistory.length;
+  if (len > 0) {
+    ctx.putImageData(canvasHistory[len - 1], 0, 0);
+  }
+}
+
+/**
+ * 撤销
+ */
+function undoCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const len = canvasHistory.length;
+  if (len > 0) {
+    ctx.putImageData(canvasHistory.pop(), 0, 0);
+  }
+}
+
+/**
+ * 清空画布
+ */
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  saveCanvas();
+}
+
+/**
+ * 加载图片
+ */
+function loadImage(url) {
+  const img = new Image();
+  img.src = url;
+  img.onload = function () {
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    saveCanvas();
+  };
 }
